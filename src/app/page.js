f98b5cf52b8ca7506333d6ebba6ef9024c16ed4e
AkimT13 <akimtarasov@gmail.com>
2024-06-03 16:55:01 -0700

    fabricated company data

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col w-full items-center justify-between p-24 bg-gradient-to-r from-slate-500 to-slate-800">
-      <p className=" text-center whitespace-nowrap text-2xl md:text-7xl text-white font-bold">Business Solutions Architect</p>
+      <p className=" text-center whitespace-nowrap text-2xl md:text-6xl text-gray-300 font-bold">Business Solutions Architect</p>
 
     </main>
   );
